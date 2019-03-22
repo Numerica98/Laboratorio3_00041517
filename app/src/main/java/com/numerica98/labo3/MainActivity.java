ae428be8c0eae20b80fec155d665dc33f628861b
Numerica98 <00041517@uca.edu.sv>
2019-03-22 11:58:36 -0600

    CAmbio en la interfaz de la segunda ventana

--- app/src/main/java/com/numerica98/labo3/MainActivity.java
+++ app/src/main/java/com/numerica98/labo3/MainActivity.java
@@ -5,17 +5,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button m_action_submit;
+    EditText editText1, editText2, editText3, editText4;
+    TextView textView1, textView2, textView3, textView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        m_action_submit= findViewById(R.id.submit_action);
+        m_action_submit= findViewById(R.id.boton_submit);
+        editText1= findViewById(R.id.editText_user);
+        editText2= findViewById(R.id.editText_password);
+        editText3= findViewById(R.id.editText_email);
+        editText4= findViewById(R.id.editText_gender);
 
         m_action_submit.setOnClickListener(new View.OnClickListener() {
             @Override
