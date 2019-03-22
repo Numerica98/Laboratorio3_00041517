d4d4ad3acbccd4cecaecbdb0444db0232ea62056
Numerica98 <00041517@uca.edu.sv>
2019-03-22 08:50:22 -0600

    Clases agregadas

---
+++ app/src/main/java/com/numerica98/labo3/AppConstants.java
@@ -0,0 +1,5 @@
+package com.numerica98.labo3;
+
+public class AppConstants {
+    public static final String TEXT1_KEY= "Laboratorio1";
+}
---
+++ app/src/main/java/com/numerica98/labo3/ShareActivity.java
@@ -0,0 +1,31 @@
+package com.numerica98.labo3;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ShareActivity extends AppCompatActivity {
+
+    TextView mSharedText;
+    private String text_aux;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_share);
+
+        mSharedText= findViewById(R.id.sharedText);
+
+        text_aux= "Texto quemado";
+
+        Intent new_intent = getIntent();
+
+        if(new_intent != null){
+            text_aux= new_intent.getStringExtra(Intent.EXTRA_TEXT);
+
+        }
+
+        mSharedText.setText(text_aux);
+    }
+}
